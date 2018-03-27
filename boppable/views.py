3689168dd31df694cef56126b1f987a013f90527
Emmet <emmetdoyle@ymail.com>
2018-03-27 12:53:16 +0100

    Update does_track_voting_exist() to check request body

--- boppable/views.py
+++ boppable/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -72,8 +73,9 @@ def track_voting_downvote(request, pk):
 
 @csrf_exempt
 def does_track_voting_exist(request):
-    track_id = request.POST['track_id']
-    playlist = request.POST['playlist']
+    body = json.loads(request.body)
+    track_id = body['track_id']
+    playlist = body['playlist']
 
     exists = TrackVoting.objects.filter(playlist_id=playlist, track_id=track_id).exists()
 
